d33fe2633854ae211bc066b41b9d69988efea377
Andy1580 <andreslopezch1996@gmail.com>
2023-09-23 11:45:26 -0600

    Proyecto subido al GitHub

---
+++ Assets/RAT_PROJECT/SCRIPTS/RumbaFunctions.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class RumbaFunctions : MonoBehaviour
+{
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject gameOverCanvas;
+    [SerializeField] private bool isAlive;
+
+    RumbaPatrol rumbaPatrolScript;
+    Collider col;
+
+    private void Start()
+    {
+        rumbaPatrolScript = GetComponent<RumbaPatrol>();
+        col = GetComponent<Collider>();
+
+        isAlive = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (isAlive == true)
+            {
+                Destroy(playerPrefab);
+                gameOverCanvas.SetActive(true);
+            }
+        }
+
+        if (collision.gameObject.tag == "Tramp")
+        {
+            rumbaPatrolScript.enabled = false;
+            isAlive = false;
+
+        }
+    }
+
+
+}
---
+++ Assets/RAT_PROJECT/SCRIPTS/RumbaPatrol.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RumbaPatrol : MonoBehaviour
+{
+    [SerializeField] private Transform[] pointsToMove;
+    [SerializeField] private float speedMovement;
+    [SerializeField] private float maxDistance;
+
+    private int nextStep = 0;
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, pointsToMove[nextStep].position, speedMovement * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, pointsToMove[nextStep].position) < maxDistance)
+        {
+            nextStep += 1;
+            if(nextStep >= pointsToMove.Length)
+            {
+                nextStep = 0;
+            }
+        }
+    }
+}
